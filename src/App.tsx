f2976f6c389bc197471f1b74e9dc7cedbd6493f0
Dîyar <didad9683@gmail.com>
2026-05-04 11:11:26 -0700

    Add keys to rendered components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,19 +13,19 @@ function AppContent() {
 
   const renderPage = () => {
     switch (currentPage) {
-      case 'boot':    return <BootScreen />;
-      case 'desktop': return <Desktop />;
-      case 'gallery': return <Gallery />;
-      case 'about':   return <About />;
-      case 'contact': return <Contact />;
-      default:        return <Desktop />;
+      case 'boot':    return <BootScreen key="boot" />;
+      case 'desktop': return <Desktop key="desktop" />;
+      case 'gallery': return <Gallery key="gallery" />;
+      case 'about':   return <About key="about" />;
+      case 'contact': return <Contact key="contact" />;
+      default:        return <Desktop key="desktop-default" />;
     }
   };
 
   return (
     <div style={{ width: '100vw', height: '100vh', overflow: 'hidden', background: '#080018', position: 'relative' }}>
       {crtEnabled && <div id="crt-overlay" aria-hidden />}
-      <div style={{ position: 'absolute', inset: 0, zIndex: 1 }}>
+      <div style={{ position: 'absolute', inset: 0, zIndex: 2 }}>
         {renderPage()}
       </div>
     </div>
